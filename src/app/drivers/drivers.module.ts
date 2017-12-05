ccf9b7744e1ca492b0dca41405424a03821525e1
mracer0102 <mracer0102@gmail.com>
2017-12-06 10:45:45 +1100

    Add service

--- src/app/drivers/drivers.module.ts
+++ src/app/drivers/drivers.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DriversRoutingModule } from './drivers-routing.module';
+import { DriversService } from './drivers.service';
 
 @NgModule({
   imports: [
     CommonModule,
     DriversRoutingModule
   ],
-  declarations: []
+  declarations: [],
+  providers: [DriversService]
 })
 export class DriversModule { }
