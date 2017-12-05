82f9d3beb4fb99bd9ce2c6ceb773d6576ad26e39
michael-ctr <freezingdeep@yahoo.com>
2017-12-06 10:00:18 +1100

    add drivers module

---
+++ src/app/drivers/drivers.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DriversRoutingModule } from './drivers-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    DriversRoutingModule
+  ],
+  declarations: []
+})
+export class DriversModule { }
