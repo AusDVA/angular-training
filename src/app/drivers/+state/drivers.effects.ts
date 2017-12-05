8747fd11ef212f29e2c58ef2c7477f85a32d1aed
James Alan Nguyen <djtubig.malicex@gmail.com>
2017-12-06 10:57:30 +1100

    effects progress - waiting on michael and shatheesh

--- src/app/drivers/+state/drivers.effects.ts
+++ src/app/drivers/+state/drivers.effects.ts
@@ -3,56 +3,60 @@ import { switchMap, map, catchError } from 'rxjs/operators';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { Actions, Effect, toPayload } from '@ngrx/effects';
+import { DriversService } from '../drivers.service';
+// import * as DriversActions from './drivers.actions'; // uncomment once created
 
 @Injectable()
 export class DriversEffects {
-    @Effect()
-    loadDriverViewData$ = this.actions$
-        // .ofType<DriversActions.LoadDriverViewData>(DriversActions.LOAD_DRIVER_VIEW_DATA)
-        .ofType(any)
-        .pipe(
-            switchMap(action => {
-                return this.driversService.loadDriverViewData(action.payload).pipe(
-                    map(driver => {
-                        // return new DriversActions.LoadDriverViewDataAction(driver);
-                        return 'FIXME';
-                    })// ,
-                    // catchError((error: HttpErrorResponse) => {
-                    //     console.error('Fail: LoadDriverViewDataAction');
-                    //     const message =
-                    //         error.status === 404 ? 'Drivers Licence not found' : error.statusText;
-                    //     return of(new UserActions.LoadError(message));
-                    // })
-                );
-            })
+
+  constructor(private actions$: Actions, private driversService: DriversService) {}
+
+  @Effect()
+  loadDriverViewData$ = this.actions$
+    // .ofType<DriversActions.LoadDriverViewData>(DriversActions.LOAD_DRIVER_VIEW_DATA)
+    .ofType(any)
+    .pipe(
+      switchMap(action => {
+        return this.driversService.getDriverRegoInfo(action.payload).pipe(
+          map(driver => {
+            // return new DriversActions.LoadDriverViewDataAction(driver);
+            return 'FIXME';
+          }) // ,
+          // catchError((error: HttpErrorResponse) => {
+          //     console.error('Fail: LoadDriverViewDataAction');
+          //     const message =
+          //         error.status === 404 ? 'Drivers Licence not found' : error.statusText;
+          //     return of(new UserActions.LoadError(message));
+          // })
         );
+      })
+    );
 
-
-    @Effect()
-    submitDriver$ = this.actions$
-        // .ofType<DriversActions.SubmitDriver>(DriversActions.SUBMIT_DRIVER)
-        .ofType(any)
-        .pipe(
-            switchMap(action => {
-                return this.driversService.submitDriverRegistration(action.payload).pipe(
-                    map(driver => {
-                        if (driver) {
-                            // return new DriversActions.LoadDriverViewDataAction(driver);
-                            return 'FIXME';
-                        } else {
-                            // return new DriversActions.SubmitDriverSuccess(null);
-                            return 'FIXME';
-                        }
-
-                    }   )// ,
-                    // catchError((error: HttpErrorResponse) => {
-                    //     console.error('Fail: LoadDriverViewData');
-                    //     const message =
-                    //         error.status === 404 ? 'Drivers Licence not found' : error.statusText;
-                    //     return of(new UserActions.SubmitError(message));
-                    // })
-                );
-            })
-        );
-
+  @Effect()
+  submitDriver$ = this.actions$
+    // .ofType<DriversActions.SubmitDriver>(DriversActions.SUBMIT_DRIVER)
+    .ofType(any)
+    .pipe(
+      switchMap(action => {
+          // delete once created
+        return this.driversService.saveDriver(action.payload)
+          .pipe(
+            map(driver => {
+              if (driver) {
+                // return new DriversActions.LoadDriverViewDataAction(driver);
+                return 'FIXME';
+              } else {
+                // return new DriversActions.SubmitDriverSuccess(null);
+                return 'FIXME';
+              }
+            }) // ,
+            // catchError((error: HttpErrorResponse) => {
+            //     console.error('Fail: LoadDriverViewData');
+            //     const message =
+            //         error.status === 404 ? 'Drivers Licence not found' : error.statusText;
+            //     return of(new UserActions.SubmitError(message));
+            // })
+          );
+      })
+    );
 }
