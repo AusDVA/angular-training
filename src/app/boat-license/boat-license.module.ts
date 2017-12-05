35760477ae31eb8c9db3d86b047d41d37e225eb6
Jye H <jye.hewett@hotmail.com>
2017-12-06 10:24:59 +1100

    Added boating Module

---
+++ src/app/boat-license/boat-license.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BoatLicenseRoutingModule } from './boat-license-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BoatLicenseRoutingModule
+  ],
+  declarations: []
+})
+export class BoatLicenseModule { }
