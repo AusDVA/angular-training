0e63acaa101ede4e79529e94fc12770aef06df76
Fabio Farina <fabio.farina92@gmail.com>
2017-12-06 12:49:39 +1100

    Add error handling method

--- src/app/boat-license/boat-license.service.ts
+++ src/app/boat-license/boat-license.service.ts
@@ -3,6 +3,8 @@ import { Observable } from 'rxjs/Observable';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { BoatLicenseApplication } from './models/boat-license-application.model';
+import { EmptyObservable } from 'rxjs/observable/EmptyObservable';
+import { catchError, delay } from 'rxjs/operators';
 
 @Injectable()
 export class BoatLicenseService {
@@ -18,7 +20,13 @@ export class BoatLicenseService {
   submitBoatLicense(boatLicense: BoatLicenseApplication): Observable<BoatLicenseApplication | any> {
       return this.httpClient.post<BoatLicenseApplication>(
         `${environment.apiUrl}/`, boatLicense, { headers: new HttpHeaders().set('content-type', 'application/json') }
-      );
+      )
+      .pipe(catchError(this.errorHandler));
+  }
+
+  private errorHandler(error: Error): Observable<BoatLicenseApplication> {
+    console.error('USER ERROR:', error);
+    return new EmptyObservable();
   }
 
 }
