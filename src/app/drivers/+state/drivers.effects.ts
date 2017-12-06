f801aea923506e84546419a2d96ff3677b365a36
Alex Lee <zhuoyanlee@gmail.com>
2017-12-06 15:42:36 +1100

    Dispatch action to get rego

--- src/app/drivers/+state/drivers.effects.ts
+++ src/app/drivers/+state/drivers.effects.ts
@@ -18,24 +18,24 @@ export class DriversEffects {
   ) {}
 
   // commented out until dashboard is created
-  //   @Effect()
-  //   loadDriverViewData$ = this.actions$
-  //     .ofType<DriversActions.LoadDriverViewDataAction>(DriversActions.LOAD_DRIVER_VIEW_DATA)
-  //     .pipe(
-  //       switchMap(action => {
-  //         return this.driversService.getDriverRegoInfo(action.payload).pipe(
-  //           map(driver => {
-  //             return new DriversActions.LoadDriverViewDataAction(driver);
-  //           }),
-  //           catchError((error: HttpErrorResponse) => {
-  //               console.error('Fail: LoadDriverViewDataAction');
-  //               const message =
-  //                   error.status === 404 ? 'Drivers Licence not found' : error.statusText;
-  //               return of(new DriversActions.SubmitErrorAction(message));
-  //           })
-  //         );
-  //       })
-  //     );
+  @Effect()
+  loadDriverViewData$ = this.actions$
+    .ofType<DriversActions.LoadDriverViewDataAction>(DriversActions.LOAD_DRIVER_VIEW_DATA)
+    .pipe(
+      switchMap(action => {
+        return this.driversService.getDriverRegoInfo(action.payload.ID).pipe(
+          map(driver => {
+            return new DriversActions.LoadDriverViewDataAction(driver);
+          }),
+          catchError((error: HttpErrorResponse) => {
+              console.error('Fail: LoadDriverViewDataAction');
+              const message =
+                  error.status === 404 ? 'Drivers Licence not found' : error.statusText;
+              return of(new DriversActions.SubmitErrorAction(message));
+          })
+        );
+      })
+    );
 
   @Effect()
   submitDriver$ = this.actions$
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CardComponent } from './card/card.component';
 import { DriversService } from '../drivers/drivers.service';
+import { Observable } from 'rxjs/Observable';
+import { DriversLicenceRegistration } from '../drivers/models/driversLicenceRegistration';
+import { Store } from '@ngrx/store';
+import { State } from '../state';
+import { LoadDriverViewDataAction } from '../drivers/+state/drivers.actions';
+import * as DriverActions from '../drivers/+state/drivers.actions';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'ngat-home',
@@ -9,13 +16,24 @@ import { DriversService } from '../drivers/drivers.service';
 })
 export class HomeComponent implements OnInit {
 
+  drivers$: Observable<DriversLicenceRegistration> ;
+  drivers: DriversLicenceRegistration;
   imports = [
     CardComponent
   ];
-  constructor(private driversService: DriversService) { }
+  constructor(private driversService: DriversService, private store: Store<State>, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    // getDriverRegos$ = this.driversService.getAllDriverRegoInfo();
+    this.driversService.getAllDriverRegoInfo().subscribe(drivers => {
+      this.drivers = drivers;
+    });
+
+    this.activatedRoute.params.subscribe(params => {
+      const id = +params['id']; // (+) converts string 'id' to a number
+      if (id) {
+        this.store.dispatch(new DriverActions.LoadDriverViewDataAction(id));
+      }
+    });
   }
 
 }
