b0d5fcf4849ac39d4506d20e9f2181e8e9cb5f0d
Fabio Farina <fabio.farina92@gmail.com>
2017-12-06 14:11:02 +1100

    Initial work on service testing

--- src/app/boat-license/boat-license.service.spec.ts
+++ src/app/boat-license/boat-license.service.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { environment } from '../../environments/environment';
 
 import { BoatLicenseService } from './boat-license.service';
 
 describe('BoatLicenseService', () => {
+
+  let boatLicenseService: BoatLicenseService;
+  let httpMock: HttpTestingController;
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [BoatLicenseService]
     });
   });
