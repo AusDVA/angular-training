7b474e132e443065128edf804864429c3bdb6431
Alex Lee <zhuoyanlee@gmail.com>
2017-12-06 14:21:59 +1100

    Add card component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,7 +15,7 @@ export class HomeComponent implements OnInit {
   constructor(private driversService: DriversService) { }
 
   ngOnInit() {
-    //getDriverRegos$ = this.driversService.getAllDriverRegoInfo();
+    // getDriverRegos$ = this.driversService.getAllDriverRegoInfo();
   }
 
 }
