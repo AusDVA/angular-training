b78f59be94cb014e9cb910ad96352cc1e6a8ed94
Belinda Ding <belinda.ding.bd@gmail.com>
2017-12-06 11:49:49 +1100

    fix import

--- src/app/boat-license/boat-license.service.ts
+++ src/app/boat-license/boat-license.service.ts
@@ -2,16 +2,15 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { BoatLicense } from './models/boat-license-application.model';
+import { BoatLicenseApplication } from './models/boat-license-application.model';
 
 @Injectable()
 export class BoatLicenseService {
 
   constructor(private httpClient: HttpClient) { }
 
-
-  submitBoatLicense(boatLicense: BoatLicense): Observable<BoatLicense | any> {
-      return this.httpClient.post<BoatLicense>(
+  submitBoatLicense(boatLicense: BoatLicenseApplication): Observable<BoatLicenseApplication | any> {
+      return this.httpClient.post<BoatLicenseApplication>(
         `${environment.apiUrl}/`, boatLicense, { headers: new HttpHeaders().set('content-type', 'application/json') }
       );
   }
