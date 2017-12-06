acc6f097f4542fca5bd44e781633cb15e2a0e05b
Fabio Farina <fabio.farina92@gmail.com>
2017-12-06 14:28:51 +1100

    Merge hell.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
-import { BoatLicenseModule } from './boat-license/boat-license.module';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './lib/material/material.module';
+import { DriversModule } from './drivers/drivers.module';
+import { BoatLicenseModule } from './boat-license/boat-license.module';
+
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 
 
 
@@ -21,8 +26,15 @@ import { MaterialModule } from './lib/material/material.module';
     AppRoutingModule,
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
+    NoopAnimationsModule,
     MaterialModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    MaterialModule,
+    DriversModule,
+    BoatLicenseModule,
     BoatLicenseModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
