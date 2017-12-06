ef89bb33dab9d3b8a29f376d4b3781ecbaa7f532
michael-ctr <freezingdeep@yahoo.com>
2017-12-06 11:09:57 +1100

    driver actions

--- src/app/drivers/+state/drivers.actions.ts
+++ src/app/drivers/+state/drivers.actions.ts
@@ -1,7 +1,34 @@
 import { Action } from '@ngrx/store';
+import { DriversLicenceRegistration } from '../models/drivers';
+
+export const SUBMIT = '[Drivers] Sumbit';
+export const SUBMIT_ERROR = '[Drivers] SumbitError';
+export const SUBMIT_SUCCESS = '[Drivers] SumbitSUCCESS';
+export const LOAD_DRIVER_VIEW_DATA = '[Drivers] LoadDriverViewData';
 
 export class SubmitAction implements Action {
-    readonly type = 'DFgd';
+    readonly type = SUBMIT;
+    constructor(public payload: DriversLicenceRegistration) { }
 }
 
-export type ActionTypes = SubmitAction;
+export class SubmitErrorAction implements Action {
+    readonly type = SUBMIT_ERROR;
+    constructor(public payload: string) { }
+}
+
+export class SubmitSuccessAction implements Action {
+    readonly type = SUBMIT_SUCCESS;
+    constructor(public payload: null) { }
+}
+
+export class LoadDriverViewDataAction implements Action {
+    readonly type = LOAD_DRIVER_VIEW_DATA;
+    constructor(public payload: DriversLicenceRegistration) { }
+}
+
+
+
+export type ActionTypes = SubmitAction
+                          |SubmitErrorAction
+                          |SubmitSuccessAction
+                          |LoadDriverViewDataAction;
